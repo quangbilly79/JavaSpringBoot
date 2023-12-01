615ffe3ad0cb4b05913a9007c5a9f4199cbe6ea8
quangbilly79 <quang.billy.79@gmail.com>
2023-12-01 11:01:16 +0700

    20231201 vega

---
+++ src/main/java/gamerunning/MarioGame.java
@@ -0,0 +1,19 @@
+package gamerunning;
+
+// Game Mario
+public class MarioGame {
+
+    // Các method di chuyển
+    public void up() {
+        System.out.println("Jumping up");
+    }
+    public void down() {
+        System.out.println("Get into the hole");
+    }
+    public void left() {
+        System.out.println("Slow down");
+    }
+    public void right() {
+        System.out.println("Accelerate");
+    }
+}
